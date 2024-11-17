6fc72ad92884bbbdea6ffe9b6fc125d87f03e933
plxtokyonight <plxtokyonight@gmail.com>
2024-11-17 07:16:15 +0300

    deleted.

--- apps/admin_api/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AdminApiModule } from './admin_api.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AdminApiModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
--- apps/email_processor/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { EmailProcessorModule } from './email_processor.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(EmailProcessorModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
